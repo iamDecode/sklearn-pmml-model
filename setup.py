c0973c8644cadd473c79611f2f00413dcee66f6b
Dennis Collaris <d.collaris@me.com>
2019-01-31 16:13:24 +0100

    chore: Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-import setuptools
+from setuptools import setup, find_packages, Extension
 import numpy as np
-from distutils.extension import Extension
-from Cython.Build import cythonize
+from Cython.Build import cythonize, build_ext
 
 with open("README.md", "r") as fh:
   long_description = fh.read()
 
-setuptools.setup(
+setup(
   name="sklearn-pmml-model",
-  version="0.0.1",
+  version="0.0.2",
   author="Dennis Collaris",
   author_email="d.collaris@me.com",
   description="A library to parse PMML models into Scikit-learn estimators.",
   long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/iamDecode/sklearn-pmml-model",
-  packages=setuptools.find_packages(),
+  packages=find_packages(),
   classifiers=(
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: BSD License",
@@ -44,5 +43,6 @@ setuptools.setup(
     Extension("sklearn_pmml_model.tree._criterion", ["sklearn_pmml_model/tree/_criterion.pyx"], include_dirs=[np.get_include()]),
     Extension("sklearn_pmml_model.tree._splitter", ["sklearn_pmml_model/tree/_splitter.pyx"], include_dirs=[np.get_include()]),
     Extension("sklearn_pmml_model.tree._utils", ["sklearn_pmml_model/tree/_utils.pyx"], include_dirs=[np.get_include()]),
-  ])
+  ]),
+  build_ext = build_ext
 )
