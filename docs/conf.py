021819fa9d8d48860b4f8659830dc029e4fcdbc3
Dennis Collaris <d.collaris@me.com>
2019-07-05 18:05:32 +0200

    docs: update sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,8 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+master_doc = 'index'
+
 # -- AutoAPI -----------------------------------------------------------------
 autoapi_type = 'python'
 autoapi_dirs = ['../sklearn_pmml_model']
