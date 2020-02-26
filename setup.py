5aacb1e9ab5c0d2560433049b52f7aa58dfdd3f9
Dennis Collaris <d.collaris@me.com>
2020-02-26 13:32:39 +0100

    chore: release 0.0.13

--- setup.py
+++ setup.py
@@ -163,7 +163,7 @@ my_ext_modules = cythonize(cython_ext_modules, include_path=my_include_dirs, gdb
 
 setup(
   name="sklearn-pmml-model",
-  version="0.0.12",
+  version="0.0.13",
   author="Dennis Collaris",
   author_email="d.collaris@me.com",
   description = "A library to parse PMML models into Scikit-learn estimators.",
